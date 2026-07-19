b0209660dc42c3334f1a1e33c4e4494e44f1e340
AW1270 <aoiwakabayashi0@outlook.jp>
2026-07-19 13:17:10 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    speed: 600,
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
